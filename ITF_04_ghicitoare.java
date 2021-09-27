42baa4ea3636d5e35d2337d2644508204a26c83f
Andy <88882350+Andy-ITFactory@users.noreply.github.com>
2021-09-27 15:09:25 +0300

    Update ITF_04_ghicitoare.java

--- ITF_04_ghicitoare.java
+++ ITF_04_ghicitoare.java
@@ -7,19 +7,19 @@ public class Main {
         // joculet ghicitoare numar secret care leaga tot ce am invatat pana acum
         // ghiceste un numar intre 1-30, afiseaza hint-uri < sau mai >
 
-        int numar = 5;
+        int numar_secret = 5;
         int numar_ghicit = 0;
+        
+        System.out.println("Ghiceste un numar intre 1-30");
 
-        while (numar_ghicit != numar){ // atat timp cat inca nu s-a ghicit nr, continua jocul
-
-            System.out.println("Ghiceste un numar intre 1-30");
+        while (numar_ghicit != numar_secret){ // atat timp cat inca nu s-a ghicit nr, continua jocul
 
             Scanner scanner = new Scanner(System.in); // avem nevoie de scanner ca sa preluam date de la tastatura
             numar_ghicit = scanner.nextInt(); // luam urmatorul numar de la tastatura
 
-            if ( numar_ghicit == numar){ // daca numarul este acelasi
+            if (numar_ghicit == numar_secret){ // daca numarul este acelasi
                 System.out.println("Felicitari"); // jucatorul castiga
-            } else if (numar_ghicit < numar){ // altfel, daca nr_ghicit < nr, dam un indiciu jucatorului
+            } else if (numar_ghicit < numar_secret){ // altfel, daca nr_ghicit < nr, dam un indiciu jucatorului
                 System.out.println("Alege un nr mai mare");
             } else { // daca nu e egal, nici mai mic, automat e mai mare, nu avem nevoie de conditie explicita
                 System.out.println("Alege un nr mai mic");
